10f3d0625be3eb09479d97966fc7cd1e9c4b923e
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-02-17 21:12:09 +0700

    Upd: switch app.js to typescript

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
-const menu = document.querySelector(".menu") as HTMLElement;
-const operators = document.querySelector(".operators") as HTMLElement;
-const closeApp = document.querySelector(".close-app") as HTMLElement;
-
+"use strict";
+const menu = document.querySelector(".menu");
+const operators = document.querySelector(".operators");
+const closeApp = document.querySelector(".close-app");
 menu.onclick = () => {
     operators.style.display = "flex";
     closeApp.style.display = "block";
     menu.style.display = "none";
-}
-
+};
 closeApp.onclick = () => {
     operators.style.display = "none";
     closeApp.style.display = "none";
     menu.style.display = "block";
-}
+};
